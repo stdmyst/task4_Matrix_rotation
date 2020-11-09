594f03c7dbc1d1623682f958b0ffcff199977c47
Vladimir <73358998+stdmyst@users.noreply.github.com>
2020-11-09 14:29:20 +0300

    Made corrections

--- task4_Matrix_rotation/Source.cpp
+++ task4_Matrix_rotation/Source.cpp
@@ -15,63 +15,63 @@ int main() {
 	int k = 0;
 
 	cin >> x;
-	for (int i = 0; i < 3; i++)
+	for (size_t i = 0; i < 3; i++)
 	{
-		for (int j = 0; j < 3; j++)
+		for (size_t j = 0; j < 3; j++)
 		{
 			cin >> arr1[i][j];
 		}
 	}
 
-		while (x > x0)
+	while (x > x0)
+	{
+		k = 2;
+		for (size_t i = 0; i < 3; i++)
+		{
+			for (size_t j = 0; j < 3; j++)
+			{
+				arr2[j][k] = arr1[i][j];
+			}
+			k--;
+		}
+		for (size_t i = 0; i < 3; i++)
+		{
+			for (size_t j = 0; j < 3; j++)
+			{
+				arr1[i][j] = arr2[i][j];
+			}
+		}
+		x--;
+	}
+
+	while (x < x0)
+	{
+		for (size_t i = 0; i < 3; i++)
 		{
 			k = 2;
-			for (int i = 0; i < 3; i++)
+			for (size_t j = 0; j < 3; j++)
 			{
-				for (int j = 0; j < 3; j++)
-				{
-					arr2[j][k] = arr1[i][j];
-				}
+				arr2[k][i] = arr1[i][j];
 				k--;
 			}
-			for (int i = 0; i < 3; i++)
-			{
-				for (int j = 0; j < 3; j++)
-				{
-					arr1[i][j] = arr2[i][j];
-				}
-			}
-			x--;
 		}
-
-		while (x < x0)
+		for (size_t i = 0; i < 3; i++)
 		{
-			for (int i = 0; i < 3; i++)
+			for (size_t j = 0; j < 3; j++)
 			{
-				k = 2;
-				for (int j = 0; j < 3; j++)
-				{
-					arr2[k][i] = arr1[i][j];
-					k--;
-				}
+				arr1[i][j] = arr2[i][j];
 			}
-			for (int i = 0; i < 3; i++)
-			{
-				for (int j = 0; j < 3; j++)
-				{
-					arr1[i][j] = arr2[i][j];
-				}
-			}
-			x++;
 		}
+		x++;
+	}
 
-	for (int i = 0; i < 3; i++)
+	for (size_t i = 0; i < 3; i++)
 	{
-		for (int j = 0; j < 3; j++)
+		cout << "\n";
+		for (size_t j = 0; j < 3; j++)
 		{
 			cout << arr1[i][j] << " ";
 		}
-		cout << "\n";
 	}
 	return 0;
 }
